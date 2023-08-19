34d6397af883233d4c981410e7dc82a0ed49a9cf
Wdvh <86887912+Wdvh@users.noreply.github.com>
2023-08-19 12:01:20 +0200

    added returning

--- pmm_main_run.sh
+++ pmm_main_run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+python3 /Users/silviovolley/backupyml/returning.py
 python3 plex_meta_manager.py --config config/config_mass_updates.yml --run 
 python3 plex_meta_manager.py --config config/config_metadata_update.yml --run 
 python3 plex_meta_manager.py --config config/config.yml --run
